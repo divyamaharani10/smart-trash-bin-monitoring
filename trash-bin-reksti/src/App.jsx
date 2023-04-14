b5288cd7d2169378b40ef8072647cc62099d6f4f
divyamaharani21 <114225819+divyamaharani10@users.noreply.github.com>
2023-04-14 13:51:25 +0700

    login page

---
+++ trash-bin-reksti/src/App.jsx
@@ -0,0 +1,68 @@
+
+import { useState } from "react";
+import "./App.css";
+import FormInput from "./views/login";
+
+const App = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    
+    {
+      id: 2,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Masuk ke Akun Anda</h1>
+        <text>Pantau dan kelola sampah secara mudah!</text>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Masuk</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
