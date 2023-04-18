c282322825d296b7b5c9f4d588f7c2de435d404d
dhiyarisalah <18220047@std.stei.itb.ac.id>
2023-04-18 10:27:28 +0700

    fixed

--- trash-bin-reksti/src/App.jsx
+++ trash-bin-reksti/src/App.jsx
@@ -1,12 +1,26 @@
-
-import { useState } from "react";
-import "./App.css";
-import Map from "./views/Map";
+import { useState } from "react"
+import "./App.css"
+import LoginPage from "./views/login_page"
+import BinLevel from "./views/binLevel"
+import BinMap from "./views/Map"
+// import Test from "./views/testFirebase"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
 
   return (
-    <Map/>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/map" element={<BinMap />}/>
+        <Route path="/level" element={<BinLevel />}/>
+        {/* <Route path="/test" element={<Test />}/> */}
+      </Routes>
+    </Router>
+    // <div>
+      // <LoginPage/>
+      // <BinLevel/>
+    // {/* </div> */}
   );
 };
 
